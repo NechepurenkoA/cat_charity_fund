e2429c8ab39f0c94497136a0aa36d748e76cbde5
NechepurenkoA <nechepurenko.work@gmail.com>
2023-11-06 01:22:17 +0300

    small changes to logger

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -1,8 +1,12 @@
 import logging
 
+FILE_LOCATION_ROUTE = 'app/logger/logs.txt'
+LINES_FORMAT = '%(name)s %(asctime)s %(levelname)s %(message)s'
+
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-log_handler = logging.FileHandler('app/logger/logs.txt', mode='w', encoding='utf-8')
-log_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+log_handler = logging.FileHandler(FILE_LOCATION_ROUTE, mode='w', encoding='utf-8')
+log_formatter = logging.Formatter(LINES_FORMAT)
 log_handler.setFormatter(log_formatter)
 logger.addHandler(log_handler)
